c5ed90e091e13e867588a5705fe0f402e15f6590
deniswebsites <117104632+deniswebsites@users.noreply.github.com>
2023-09-10 17:39:09 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -150,6 +150,169 @@ $(document).ready(function() {
     }
   });
 
+  // --------------
+
+    if( $(".selection_slider").length > 0 ) {
+      $(".selection_slider").not(".slick-initialized").slick({
+          dots: false,
+          arrows: true,
+          // autoplay: true,
+          autoplaySpeed: 4000,
+          speed: 1200,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><img src="img/left_blue_arrow.svg" alt="" /></button>',
+          nextArrow: '<button class="slick-next" aria-label="Next" type="button"><img src="img/right_blue_arrow.svg" alt="" /></button>',
+          // fade: true,
+          // responsive: [
+          //     {
+          //       breakpoint: 900,
+          //       settings: {
+          //         slidesToShow: 2,
+          //         slidesToScroll: 2
+          //       }
+          //     },
+          //     {
+          //       breakpoint: 540,
+          //       settings: {
+          //         slidesToShow: 1,
+          //         slidesToScroll: 1
+          //       }
+          //     }
+          //   ]
+      });
+    }
+
+    // ----------------
+
+      if( $(".testimonial_slider_selection").length > 0 ) {
+        $(".testimonial_slider_selection").not(".slick-initialized").slick({
+            dots: false,
+            arrows: true,
+            // autoplay: true,
+            autoplaySpeed: 4000,
+            speed: 1200,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><img src="img/left_blue_arrow.svg" alt="" /></button>',
+            nextArrow: '<button class="slick-next" aria-label="Next" type="button"><img src="img/right_blue_arrow.svg" alt="" /></button>',
+            // fade: true,
+            // responsive: [
+            //     {
+            //       breakpoint: 900,
+            //       settings: {
+            //         slidesToShow: 2,
+            //         slidesToScroll: 2
+            //       }
+            //     },
+            //     {
+            //       breakpoint: 540,
+            //       settings: {
+            //         slidesToShow: 1,
+            //         slidesToScroll: 1
+            //       }
+            //     }
+            //   ]
+        });
+      }
+
+      if( $(".popular_slider").length > 0 ) {
+        $(".popular_slider").not(".slick-initialized").slick({
+            dots: false,
+            arrows: true,
+            // autoplay: true,
+            autoplaySpeed: 4000,
+            speed: 1200,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"><img src="img/left_orange_arrow.svg" alt="" /></button>',
+            nextArrow: '<button class="slick-next" aria-label="Next" type="button"><img src="img/right_orange_arrow.svg" alt="" /></button>',
+            // fade: true,
+            // responsive: [
+            //     {
+            //       breakpoint: 900,
+            //       settings: {
+            //         slidesToShow: 2,
+            //         slidesToScroll: 2
+            //       }
+            //     },
+            //     {
+            //       breakpoint: 540,
+            //       settings: {
+            //         slidesToShow: 1,
+            //         slidesToScroll: 1
+            //       }
+            //     }
+            //   ]
+        });
+      }
+
+    // ---------------
+
+    $(document).on("click", "[data-popup-link]",  function(e) {
+      e.preventDefault();
+      popupName = $(this).attr("data-popup-link");
+      div = document.createElement('div');
+      div.style.overflowY = 'scroll';
+      div.style.width = '50px';
+      div.style.height = '50px';
+      div.style.visibility = 'hidden';
+      document.body.appendChild(div);
+      scrollWidth = div.offsetWidth - div.clientWidth;
+      document.body.removeChild(div);
+      topCoord = $(document).scrollTop();
+      $("body").addClass("fixed");
+      $("body").css({
+          "top" :  -1 * topCoord + "px",
+          "padding-right" : scrollWidth + "px"
+      });
+      $(".popup_bg").fadeIn(300);
+      $("[data-popup = '"+ popupName +"']").fadeIn(300);
+    });
+    $(document).on("click", ".close, .popup_bg", function(e) {
+      e.preventDefault();
+      curTop = $("body").css("top");
+      curTop = Math.abs(parseInt(curTop, 10));
+      $("body").removeClass("fixed");
+      if (curTop !== 0) {
+          $("html").scrollTop(curTop);
+      }
+      $("body").attr("style", "");
+      $("[data-popup]").fadeOut(300);
+      $(".popup_bg").fadeOut(300);
+    });
+    $(this).keydown(function(eventObject){
+      if (eventObject.which == 27 && $("body").hasClass("fixed")) {
+        curTop = $("body").css("top");
+        curTop = Math.abs(parseInt(curTop, 10));
+        $("body").removeClass("fixed");
+        if (curTop !== 0) {
+            $("html").scrollTop(curTop);
+        }
+        $("body").attr("style", "");      
+        $(".popup_bg").fadeOut(300);
+        $("[data-popup]").fadeOut(300);
+      }
+    });
+    $(document).on("mouseup", function(e) {
+      if($(".popup").is(":visible")) {
+        e.preventDefault();
+        hide_element = $(".popup_content");
+        if (!hide_element.is(e.target)
+            && hide_element.has(e.target).length === 0) {
+            curTop = $("body").css("top");
+            curTop = Math.abs(parseInt(curTop, 10));
+            $("body").removeClass("fixed");
+            if (curTop !== 0) {
+                $("html").scrollTop(curTop);
+            }
+            $("body").attr("style", "");    
+            $(".popup_bg").fadeOut(300);
+            $("[data-popup]").fadeOut(300);
+        }
+      }
+    });
+
 });
 
 });
